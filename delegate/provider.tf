87a8be6872069141c3168d7ee6e03b58f19b3cbb
saitejatamiri <98472448+saitejatamiri@users.noreply.github.com>
2025-06-23 13:40:09 +0530

    Update provider.tf

--- delegate/provider.tf
+++ delegate/provider.tf
@@ -6,6 +6,14 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_eks_cluster" "eks" {
+  name = "my-eks-cluster"  # <-- Replace this with your EKS cluster name
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = data.aws_eks_cluster.eks.name
+}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
